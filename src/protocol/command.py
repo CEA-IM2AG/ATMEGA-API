f56545f8b304f821a90d7057a259ba24f25bbd2c
kugge <sofiane.djerbi38@gmail.com>
2022-04-07 12:10:53 +0100

    Added simple command error handler

--- src/protocol/command.py
+++ src/protocol/command.py
@@ -5,8 +5,20 @@
 from enum import Enum
 
 
+class CommandError(Exception):
+    """ Any error related to hardware ports """
+    def __init__(self, code, message="Unknown Port Error"):
+        """ Init the error message """
+        self.message = message
+        self.code = hex(code)
+
+    def __str__(self):
+        """ Convert to string """
+        return f"Command {self.code} error: {self.message}"
+
 class Command(Enum):
     """ Enum of all commands """
-    INIT_RAM  = 0x55
-    WRITE_RAM = 0x22
-    READ_RAM  = 0x66    
+    RESET_RAM  = 0x55          # Reset all the ram
+    WRITE_RAM = 0x22           # Write in a single emplacement
+    READ_RAM = 0x22            # Read a single emplacement
+    READ_GROUP_RAM  = 0x66     # Read multiple emplacement
