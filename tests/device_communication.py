666dfd04db5f9c44d712ccaa832a6ca9d6247f43
lennypeers <lenny.peers@gmail.com>
2022-04-11 16:47:44 +0200

    Update README, add address check and refactor
    - Check if the address goes beyond the available memory
    - Rename functions and update return values

--- tests/device_communication.py
+++ tests/device_communication.py
@@ -3,9 +3,12 @@
     :author: Sofiane DJERBI
 """
 import logging
+
 import unittest
 from unittest import TestCase
+
 from atmega.ram import RAM
+
 
 class TestRAM(TestCase):
     def __init__(self, *args, **kwargs):
@@ -13,12 +16,16 @@ class TestRAM(TestCase):
         self.dev = RAM()
 
     def test_reset(self):
-        self.dev.reset_ram(0xF0)
-        mem = self.dev.read_group_ram(0x1000, 100)
-        self.assertEqual(mem, [0xF0 for i in range(100)])
+        self.dev.reset(0xF0)
+        mem = self.dev.read_group(0x1000, 100)
+        self.assertEqual(mem, [0xF0]*100)
     
     def test_write(self):
-        pass
+        self.dev.reset(0x32)
+        self.dev.write(0xF2, 0x331F)
+        self.assertEqual(self.dev.read(0x3320), 0x32)
+        self.assertEqual(self.dev.read(0x331E), 0x32)
+        self.assertEqual(self.dev.read(0x331F), 0xF2)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
